deca834029954d3904eb379e8686015ae53e322b
Vishnu Elayi <vishnuelayi@gmail.com>
2023-12-13 19:08:28 +0530

    added blog model

---
+++ models/blogModel.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    numViews: {
+      type: Number,
+      default: 0,
+    },
+    isLiked: {
+      type: Boolean,
+      default: false,
+    },
+    isDisLiked: {
+      type: Boolean,
+      default: false,
+    },
+    likes: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    dislikes: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    image: {
+      type: String,
+      default:
+        "https://blog.adobe.com/en/publish/2020/11/03/media_1875ab4008072aa71eb135980d7ea5ae4247ce80b.png?width=2000&format=webply&optimize=medium",
+    },
+    auther: {
+      type: String,
+      default: "Admin",
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    toObject: {
+      virtuals: true,
+    },
+    timestamps: true,
+  }
+);
+
+const Blog = mongoose.model("Blog", blogSchema);
+export default Blog;
