046433d6b93b2f7d28660295d1f5c6463fb16770
Vishnu Elayi <vishnuelayi@gmail.com>
2024-04-12 19:29:31 +0530

    completed the items on query controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,16 +1,18 @@
 import Product from "../models/productModel.js";
+import Category from "../models/categoryModel.js";
+import brandCategory from "../models/brandCatModel.js";
+import Color from "../models/colorModel.js";
 import asyncHandler from "express-async-handler";
 import slugify from "slugify";
 import User from "../models/userModel.js";
-
 
 export const createProduct = asyncHandler(async (req, res) => {
   try {
     if (req.body.title) {
       req.body.slug = slugify(req.body.title);
     }
-    const item = await Product.create(req.body)
-    
+    const item = await Product.create(req.body);
+
     res.json(item);
   } catch (error) {
     throw new Error(error);
@@ -47,7 +49,11 @@ export const deleteProduct = asyncHandler(async (req, res) => {
 export const getAproduct = asyncHandler(async (req, res) => {
   const { id } = req.params;
   try {
-    const findItem = await Product.findById(id).populate("color").populate("ratings.postedBy").populate("category").populate("brand");
+    const findItem = await Product.findById(id)
+      .populate("color")
+      .populate("ratings.postedBy")
+      .populate("category")
+      .populate("brand");
     res.json(findItem);
   } catch (error) {
     throw new Error(error);
@@ -96,7 +102,10 @@ export const getAllProducts = asyncHandler(async (req, res) => {
       const productCount = await Product.countDocuments();
       if (skip >= productCount) throw new Error("This page is not Exist");
     }
-    const product = await query.populate("color").populate("category").populate("brand")
+    const product = await query
+      .populate("color")
+      .populate("category")
+      .populate("brand");
     res.json(product);
   } catch (error) {
     throw new Error(error);
@@ -119,14 +128,14 @@ export const addToWishList = asyncHandler(async (req, res) => {
         },
         { new: true }
       );
-      res.json({message:"Removed from wishlist"});
+      res.json({ message: "Removed from wishlist" });
     } else {
       let user = await User.findByIdAndUpdate(
         _id,
         { $push: { whishlist: prodId } },
         { new: true }
       );
-      res.json({message:"Added to wishlist"});
+      res.json({ message: "Added to wishlist" });
     }
   } catch (error) {
     throw new Error(error);
@@ -190,21 +199,68 @@ export const rating = asyncHandler(async (req, res) => {
 });
 
 export const getItemOnQuery = asyncHandler(async (req, res) => {
-  try{
+  try {
     const limit = parseInt(req.query.limit) || 10;
     const startIndex = parseInt(req.query.startIndex) || 0;
 
-    const tag = req.query.tag ? {tag:{$all : req.query.tag.split(',')}} : {};
+    const category = req.query.category
+      ? {
+          category: {
+            $in: await Category.find(
+              { title: { $regex: req.query.category, $options: "i" } },
+              { _id: 1 }
+            ),
+          },
+        }
+      : {};
 
-    const item = await Product.find({...tag})
+    const color = req.query.color
+      ? {
+          color: {
+            $in: await Color.find(
+              { title: { $regex: req.query.color, $options: "i" } },
+              { _id: 1 }
+            ),
+          },
+        }
+      : {};
+
+    const brand = req.query.brand
+      ? {
+          brand: {
+            $in: await brandCategory.find(
+              { title: { $regex: req.query.brand, $options: "i" } },
+              { _id: 1 }
+            ),
+          },
+        }
+      : {};
+
+    const tag = req.query.tag
+      ? { tag: { $all: req.query.tag.split(",") } }
+      : {};
+
+    const searchTerm = req.query.searchTerm
+      ? {
+          $or: [
+            { title: { $regex: req.query.searchTerm, $options: "i" } },
+            { description: { $regex: req.query.searchTerm, $options: "i" } },
+          ],
+        }
+      : {};
+
+    const item = await Product.find({
+      ...category,
+      ...tag,
+      ...searchTerm,
+      ...color,
+      ...brand,
+    }).limit(limit);
+
     return res.json({
       item,
-      totalCount: await Product.countDocuments({...tag})
     });
-  }
-  catch(error) {
+  } catch (error) {
     throw new Error(error);
   }
-})
-
-
+});
